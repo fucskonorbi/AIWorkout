a3c0837eeaca8ba5b3f757e979b8e9519991a94f
fucskonorbi <fucskonorbi@gmail.com>
2022-10-28 22:57:34 +0200

    Add Firebase to android project

--- android_app/app/src/main/java/hu/bme/aut/android/aiworkout/MainActivity.kt
+++ android_app/app/src/main/java/hu/bme/aut/android/aiworkout/MainActivity.kt
@@ -10,11 +10,26 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.storage.FirebaseStorage
 import hu.bme.aut.android.aiworkout.ui.theme.AIWorkoutTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Create FirebaseStorage instance and get reference to the storage
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val gsReference = storage.getReferenceFromUrl("gs://aiworkout-22b29.appspot.com/Firebase/ML/Models/pose_classifier.tflite")
+        val localFile = File.createTempFile("pose_model", "tflite")
+        gsReference.getFile(localFile).addOnSuccessListener {
+            // Local temp file has been created
+            // print the file path
+            println(localFile.absolutePath)
+        }.addOnFailureListener {
+            // Handle any errors
+        }
+
         setContent {
             AIWorkoutTheme {
                 // A surface container using the 'background' color from the theme
